932dc3e7317503e13dd3efa5e5603fefe61481b3
jkcowen <jkcowen@iastate.edu>
2017-03-16 20:32:58 -0500

    Additional modifications to remove the eclipse workspace files and added initial source code that implements TCP/IP client and server along with awt.Desktop launch of the default program for the transmitted files extension.

---
+++ FileUploadTCP/Client.java
@@ -0,0 +1,71 @@
+package FileUploadTCP;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		String fileName;
+		int fileLength;
+		System.out.println("File Name: ");
+		Scanner nameInput = new Scanner(System.in);
+		fileName = nameInput.next();
+		nameInput.close();
+		
+		try {
+			// read file into byte array
+			File file = new File("C:/users/jkc11/onedrive/documents/cpre186/" + fileName);
+			fileLength = (int) file.length();
+			System.out.println("FileName: " + fileName + "  Length: " + fileLength);
+			byte[] fileBuffer = new byte[fileLength];
+			DataInputStream inFromFile = new DataInputStream(new FileInputStream(file));
+			inFromFile.read(fileBuffer);
+			inFromFile.close();
+			boolean exception = true;
+			do {
+				Socket clientSocket = null;
+				DataOutputStream outToServer = null;
+				BufferedReader inFromServer = null;
+				try {
+					// create tcp/ip socket
+					clientSocket = new Socket("127.0.0.1", 15001);
+					// create input and output streams from socket
+					outToServer = new DataOutputStream(clientSocket.getOutputStream());
+					inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+					// send file name and wait for response
+					outToServer.writeBytes(fileName+"\n");
+					System.out.println("Server recieved: " + inFromServer.readLine());
+					// send file length and wait for response
+					outToServer.writeBytes(fileLength+"\n");
+					System.out.println("Server recieved: " + inFromServer.readLine());
+					// send file byte by byte
+					outToServer.write(fileBuffer);
+					inFromServer.readLine();
+					exception = false;
+				} catch (java.net.ConnectException e) {
+//					e.printStackTrace();
+//					System.exit(0);
+					exception = true;
+				} finally{
+					if(outToServer != null)
+						outToServer.close();
+					if(inFromServer != null)
+						inFromServer.close();
+					if(clientSocket != null)
+						clientSocket.close();
+				}
+			} while (exception);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ FileUploadTCP/Server.java
@@ -0,0 +1,73 @@
+package FileUploadTCP;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.awt.Desktop;
+
+public class Server {
+
+	public static void main(String[] args) throws Exception {
+		String fileName = "";
+		String fileLength = "";
+		ServerSocket welcomeSocket = new ServerSocket(15001);
+
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataInputStream binaryInFromClient = new DataInputStream(new BufferedInputStream(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			try {
+				fileName = inFromClient.readLine();
+				System.out.println("Recieved: " + fileName);
+				outToClient.writeBytes(fileName+"\n");
+				fileLength = inFromClient.readLine();
+				System.out.println("Length: " + fileLength);
+				outToClient.writeBytes(fileLength+"\n");
+				Scanner lengthInput = new Scanner(fileLength);
+				int length = lengthInput.nextInt();
+				lengthInput.close();
+				File file = new File("C:/users/jkc11/onedrive/documents/cpre186/server/"+fileName);
+				DataOutputStream fileOutput = new DataOutputStream(new FileOutputStream(file));
+				byte[] cbuf = new byte[length];
+				int bytesRead = 0;
+				long percent = 0;
+				while (bytesRead < length) {
+					int bytesRecieved = binaryInFromClient.read(cbuf, bytesRead, length-bytesRead);
+					if (bytesRecieved > 0) {
+						bytesRead += bytesRecieved;
+						percent = 100L*bytesRead/length;
+						System.out.print("\r Percent Recieved: " + percent + "%");
+					} else
+						throw new java.lang.NullPointerException();
+				}
+				System.out.println("");
+				fileOutput.write(cbuf, 0, length);
+				fileOutput.close();
+			} catch (java.lang.NullPointerException e) {
+				System.out.println("Client intitiated disconnect.");
+				fileName = "";
+			}
+			try {
+				connectionSocket.close();
+			} finally {
+				System.out.println("Connection socket closed");
+			}
+			try{
+				//Process process = new ProcessBuilder("C:/Program Files (x86)/Windows Media Player/T-shirt.mp3").start();
+				Desktop.getDesktop().open(new File("C:/users/jkc11/onedrive/documents/cpre186/server/"+fileName));
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
